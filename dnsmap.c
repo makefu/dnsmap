4f1a078efbc1b7701e3d06b815f1291a15632e37
ap@gnucitizen.org <ap@gnucitizen.org@c94f0440-1047-0410-9073-f3f9481b7c33>
2010-01-13 18:14:15 +0000

    reverted back
    git-svn-id: http://dnsmap.googlecode.com/svn/trunk@8 c94f0440-1047-0410-9073-f3f9481b7c33

--- dnsmap.c
+++ dnsmap.c
@@ -1,6 +1,6 @@
 /*
  * ** dnsmap - DNS Network Mapper by pagvac
- * ** Copyright (C) 2010 gnucitizen.org
+ * ** Copyright (C) 2009 gnucitizen.org
  * **
  * ** This program is free software; you can redistribute it and/or modify
  * ** it under the terms of the GNU General Public License as published by
