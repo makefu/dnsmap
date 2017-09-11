2eaaf2ec4c61fdcea3d74ba69c5549ecaa6d073e
agreenbhm <agreenbhm@gmail.com>
2017-09-11 12:10:01 -0400

    Added wordlist & parallel processing

--- dnsmap-bulk.sh
+++ dnsmap-bulk.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-if [[ $# -ne 1 && $# -ne 2 ]]
+if [[ $# -ne 1 && $# -ne 2 && $# -ne 3 ]]
 then
-	echo "usage: `basename $0` <domains-file> [results-path]";
+	echo "$#";
+	echo "usage: `basename $0` <domains-file> [results-path] [wordlist-path]";
 	echo "e.g.:";
 	echo "`basename $0` domains.txt";
 	echo "`basename $0` domains.txt /tmp/";
+	echo "`basename $0` domains.txt /tmp/ /usr/share/dirbuster/wordlists/directories.jbrofuzz"; 
 	exit
 fi
 for i in `cat $1`
 do
 	if [[ $# -eq 1 ]] 
 	then	
-		dnsmap $i
-	elif [[ $# -eq 2 ]]
-	then
-		dnsmap $i -r $2
+		dnsmap $i &
+	elif [[ $# -eq 2 ]] 
+	then	
+		dnsmap $i -r $2 &
+	elif [[ $# -eq 3 ]] 
+	then	
+		dnsmap $i -r $2 -w $3 &
 	fi
-done		
+done
+wait
+
